f1fd2671a9bcd960ddfc71befb6c282dfa570d4a
Jewel Chowdhury <jewel.chowdhury@bjitgroup.com>
2022-10-18 09:02:40 +0600

    added unit testing

--- src/main/java/com/bjit/salon/service/SalonServiceApplication.java
+++ src/main/java/com/bjit/salon/service/SalonServiceApplication.java
@@ -3,10 +3,11 @@ package com.bjit.salon.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SalonServiceApplication {
 
 	public static void main(String[] args) {
