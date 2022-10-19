923d954e5dca10cde9e8eb5677583f6302aa97fb
Jewel Chowdhury <jewel.chowdhury@bjitgroup.com>
2022-10-19 11:02:19 +0600

    Refactored the test cases,and  removing unnecessary codes

--- src/test/java/com/bjit/salon/service/serviceImpl/CatalogServiceImplTest.groovy
+++ src/test/java/com/bjit/salon/service/serviceImpl/CatalogServiceImplTest.groovy
@@ -6,16 +6,19 @@ import com.bjit.salon.service.entiry.Catalog
 import com.bjit.salon.service.exception.CatalogNotFoundException
 import com.bjit.salon.service.mapper.CatalogMapper
 import com.bjit.salon.service.repository.CatalogRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
 
+@SpringBootTest
 class CatalogServiceImplTest extends Specification {
 
     private CatalogServiceImpl catalogService;
+    @Autowired
     private CatalogMapper catalogMapper;
     private CatalogRepository catalogRepository;
 
     def setup() {
-        catalogMapper = Mock(CatalogMapper)
         catalogRepository = Mock(CatalogRepository)
         catalogService = new CatalogServiceImpl(catalogRepository, catalogMapper)
     }
@@ -23,7 +26,6 @@ class CatalogServiceImplTest extends Specification {
     def "should create a new catalog"() {
 
         given:
-
         def catalogRequest = CatalogCreateDto
                 .builder()
                 .name("Normal Haircut")
@@ -41,45 +43,26 @@ class CatalogServiceImplTest extends Specification {
                 .payableAmount(100.0)
                 .build()
 
-        def catalogResponse = CatalogResponseDto
-                .builder()
-                .name("Normal Haircut")
-                .description("It's very normal hair cut for men")
-                .approximateTimeForCompletion(60)
-                .payableAmount(100.0)
-                .build()
-
-        catalogMapper.toCatalog(catalogRequest) >> catalog
-        catalogRepository.save(catalog) >> catalog
-        catalogMapper.toCatalogResponse(catalog) >> catalogResponse
+        catalogRepository.save(_) >> catalog
 
 
         when:
-        def db = catalogService.createNewCatalog(catalogRequest)
+        def catalogResponseDto = catalogService.createNewCatalog(catalogRequest)
 
         then:
-        db.getName() == "Normal Haircut"
-        db.getDescription() == "It's very normal hair cut for men"
-        db.getPayableAmount() == (double) 100.0
-        db.getApproximateTimeForCompletion() == 60
+        catalogResponseDto.getName() == "Normal Haircut"
+        catalogResponseDto.getDescription() == "It's very normal hair cut for men"
+        catalogResponseDto.getPayableAmount() == (double) 100.0
+        catalogResponseDto.getApproximateTimeForCompletion() == 60
 
     }
 
     def "should return a catalog object by catalog id"(){
 
         given:
-
         def catalog = Catalog
                 .builder()
                 .id(1L)
-                .name("Normal Haircut")
-                .description("It's very normal hair cut for men")
-                .approximateTimeForCompletion(60)
-                .payableAmount(100.0)
-                .build()
-
-        def catalogResponse = CatalogResponseDto
-                .builder()
                 .name("Normal Haircut")
                 .description("It's very normal hair cut for men")
                 .approximateTimeForCompletion(60)
@@ -87,54 +70,19 @@ class CatalogServiceImplTest extends Specification {
                 .build()
 
         catalogRepository.findById(1L) >> Optional.of(catalog)
-        catalogMapper.toCatalogResponse(catalog) >> catalogResponse
 
         when:
-        def db = catalogService.getCatalog(1L)
-
+        def catalogResponseDto = catalogService.getCatalog(1L)
 
         then:
-        db.getName() == "Normal Haircut"
-        db.getDescription() == "It's very normal hair cut for men"
-        db.getPayableAmount() == (double) 100.0
-        db.getApproximateTimeForCompletion() == 60
+        catalogResponseDto.getName() == "Normal Haircut"
+        catalogResponseDto.getDescription() == "It's very normal hair cut for men"
+        catalogResponseDto.getPayableAmount() == (double) 100.0
+        catalogResponseDto.getApproximateTimeForCompletion() == 60
 
     }
 
-    def "should return a list of catalog"(){
-        given:
-        given:
-
-        def catalog = Catalog
-                .builder()
-                .id(1L)
-                .name("Normal Haircut")
-                .description("It's very normal hair cut for men")
-                .approximateTimeForCompletion(60)
-                .payableAmount(100.0)
-                .build()
-
-        def catalogResponse = CatalogResponseDto
-                .builder()
-                .name("Normal Haircut")
-                .description("It's very normal hair cut for men")
-                .approximateTimeForCompletion(60)
-                .payableAmount(100.0)
-                .build()
-
-        def catalogs = [catalog,catalog,catalog,catalog,catalog]
-        def catalogsResponse = [catalogResponse,catalogResponse,catalogResponse,catalogResponse,catalogResponse]
-        catalogRepository.findAll() >> catalogs
-        catalogMapper.toCatalogsResponse(catalogs) >> catalogsResponse
-
-        when:
-        def size = catalogService.getAllCatalog().size()
-
-        then:
-        size == 5
-    }
-
-    def "should throw catalog not found exception"(){
+    def "should throw catalog not found exception by catalog id"(){
         given:
         catalogRepository.findById(1L) >> {  throw new CatalogNotFoundException("catalog not found") }
 
@@ -145,5 +93,29 @@ class CatalogServiceImplTest extends Specification {
         def ex = thrown(CatalogNotFoundException)
         ex.message == "catalog not found"
     }
+
+    def "should return a list of catalog"(){
+        given:
+        def catalog = Catalog
+                .builder()
+                .id(1L)
+                .name("Normal Haircut")
+                .description("It's very normal hair cut for men")
+                .approximateTimeForCompletion(60)
+                .payableAmount(100.0)
+                .build()
+
+
+        def catalogs = [catalog,catalog,catalog,catalog,catalog]
+        catalogRepository.findAll() >> catalogs
+
+        when:
+        def size = catalogService.getAllCatalog().size()
+
+        then:
+        size == 5
+    }
+
+
 
 }
